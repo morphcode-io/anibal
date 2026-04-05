96e1f627a10b8e7075d1444ee7617a52919bf4c5
Anibal Laura <morphcode.io@gmail.com>
2026-04-05 09:22:34 -0400

    feat: add NotFound component for handling 404 errors

---
+++ src/app/not-found.tsx
@@ -0,0 +1,5 @@
+export default function NotFound() {
+    return <div className="flex w-full h-svh justify-center items-center">
+        <h1 className="text-2xl font-bold">404 - Page Not Found</h1>
+    </div>;
+}
